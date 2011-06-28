070fe40b0dce7b719f956e4111dd02737abba1d0
Luke Daley <ld@ldaley.com>
2011-06-28 10:03:37 +1000

    Updated to 0.6.0

--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
-This is an example of incorporating Geb into a Gradle build.
+This is an example of incorporating Geb into a Gradle build. It shows the use of Spock and JUnit 4 tests.
 
-It shows the use of Spock and JUnit 4 tests.
+The build is setup to work with HTMLUnit, FireFox and Chrome. Have a look at the `build.gradle` and the `src/test/resources/GebConfig.groovy` files.
+
+The following commands will launch the tests with the individual browsers:
+
+    ./gradlew htmlunitTest
+    ./gradlew chromeTest
+    ./gradlew firefoxTest
+
+To run with all, you can run:
+
+    ./gradlew test
--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,5 +1,40 @@
-// Geb configuration can go in here…
-// See: http://www.gebish.org/manual/configuration.html
+/*
+	This is the Geb configuration file.
+	
+	See: http://www.gebish.org/manual/current/configuration.html
+*/
 
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.firefox.FirefoxDriver
-driver = { new FirefoxDriver() }
+import org.openqa.selenium.chrome.ChromeDriver
+
+// Use htmlunit as the default
+// run via “./gradlew htmlunitTest”
+// See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
+driver = { 
+	def driver = new HtmlUnitDriver()
+	driver.javascriptEnabled = true
+	driver
+}
+
+waiting {
+	timeout = 20
+}
+
+environments {
+	
+	// run via “./gradlew chromeTest”
+	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
+	chrome {
+		driver = { new ChromeDriver() }
+	}
+	
+	// run via “./gradlew firefoxTest”
+	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+	firefox {
+		driver = { new FirefoxDriver() }
+	}
+
+}
+
+// To run the tests with all browsers just run “./gradlew test”
