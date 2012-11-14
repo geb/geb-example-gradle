ae3ade298b906d1984b78845167f1f676409ff5f
Luke Daley <ld@ldaley.com>
2012-11-14 17:43:51 +0000

    Remove some dead code, and webdriver update.

--- src/test/groovy/GoogleHomePage.groovy
+++ src/test/groovy/GoogleHomePage.groovy
@@ -3,7 +3,4 @@ import geb.*
 class GoogleHomePage extends Page {
     static url = "http://google.com/ncr"
     static at = { title == "Googles" }
-    static content = {
-        search { module GoogleSearchModule }
-    }
 }
