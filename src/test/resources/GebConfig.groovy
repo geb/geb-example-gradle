3a1d495b19de7421e0c7923d6e731a19740bd766
Jarlaxle91 <Jarlaxle91@gmail.com>
2022-03-15 13:41:22 +1000

    Firefox was removed

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -4,10 +4,8 @@
 	See: http://www.gebish.org/manual/current/#configuration
 */
 
-
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.firefox.FirefoxDriver
 
 waiting {
     timeout = 2
@@ -15,35 +13,18 @@ waiting {
 
 environments {
 
-    // run via “./gradlew chromeTest”
-    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
     chrome {
-
         driver = { new ChromeDriver() }
     }
 
-    // run via “./gradlew chromeHeadlessTest”
-    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
     chromeHeadless {
         driver = {
             ChromeOptions o = new ChromeOptions()
-
             o.setBinary(new File("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"))
             o.addArguments('headless')
             new ChromeDriver(o)
         }
     }
-
-    // run via “./gradlew firefoxTest”
-    // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-    firefox {
-        atCheckWaiting = 1
-
-        driver = { new FirefoxDriver() }
-    }
-
 }
-
-// To run the tests with all browsers just run “./gradlew test”
 
 baseUrl = "http://gebish.org"
