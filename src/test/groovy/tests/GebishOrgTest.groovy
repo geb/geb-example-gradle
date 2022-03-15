876176402c75a0c0e71e844261e53b09cd2e3261
Jarlaxle91 <Jarlaxle91@gmail.com>
2022-03-16 00:58:09 +1000

    Firefox was removed

--- src/test/groovy/tests/GebishOrgTest.groovy
+++ src/test/groovy/tests/GebishOrgTest.groovy
@@ -6,14 +6,15 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import pages.GebishOrgHomePage
 import pages.TheBookOfGebPage
+import steps.AuthSteps
 
 @RunWith(JUnit4)
 class GebishOrgTest extends GebReportingTest {
 
     @Test
     void canGetToTheCurrentBookOfGeb() {
-        to GebishOrgHomePage
-
+//        to GebishOrgHomePage
+        AuthSteps.gotoHomePage()
         manualsMenu.open()
 
         //first link is for the current manual
