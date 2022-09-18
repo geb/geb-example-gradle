3096ea8704de76a7bfbca9238fad68f9f2e64490
Marcin Erdmann <erdi84@gmail.com>
2022-09-18 12:24:55 +0200

    Switch test tasks to run tests using JUnit Platform so that Spock 2.0 based tests are executed. Switch JUnit tests to JUnit 5.
    Fixes geb/issues#671.

--- src/test/groovy/GebishOrgTest.groovy
+++ src/test/groovy/GebishOrgTest.groovy
@@ -1,9 +1,6 @@
-import geb.junit4.GebReportingTest
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
+import geb.junit5.GebReportingTest
+import org.junit.jupiter.api.Test
 
-@RunWith(JUnit4)
 class GebishOrgTest extends GebReportingTest {
 
     @Test
